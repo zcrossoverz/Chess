48dfdc4e4ec6d3590e9fc8ae05e85bdfb454b8bd
zcrossoverz <56151228+zcrossoverz@users.noreply.github.com>
2021-10-30 22:44:29 +0700

    setup promotion pawn

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, {useEffect, useState} from "react";
-import {gameSubject} from "./Game";
+import {gameSubject,initGame} from "./Game";
 import Board from "./Board";
 import "./Style.css";
 
 function App() {
   const [board, setBoard] = useState([]);
   useEffect(() => {
+    initGame();
     const subscribe = gameSubject.subscribe(game => setBoard(game.board));
     return () => subscribe.unsubscribe();
   }, []);
--- src/Game.js
+++ src/Game.js
@@ -1,16 +1,49 @@
 import * as Chess from "chess.js";
 import {BehaviorSubject} from "rxjs";
 
-const chess = new Chess();
+let promotion = 'rnb2bnr/pppPkppp/8/4p3/7q/8/PPPP1PPP/RNBQKBNR w KQ - 1 5';
+const chess = new Chess(promotion);
 
 export const gameSubject = new BehaviorSubject({
     board: chess.board()
 });
 
-export function move(from, to) {
+export function initGame() {
+    updateGame();
+}
+
+export function handleMove(from, to) {
+    const promotions = chess.moves({verbose:true}).filter(m => m.promotion);
+    console.table(promotions);
+    if(promotions.some(p => `${p.from}:${p.to}` === `${from}:${to}`)){
+        console.log('ok');
+        const pendingPromotion = {from, to, color: promotions[0].color};
+        updateGame(pendingPromotion);
+    }
+    const {pendingPromotion} =  gameSubject.getValue();
+    if(!pendingPromotion){
+        move(from, to);
+    }
+}
+
+export function move(from, to, promotion) {
+    let tempMove = {from, to};
+    if(promotion){
+        tempMove.promotion = promotion;
+    }
     // console.log(from,to);
-    const legalMove = chess.move({from,to});
+    const legalMove = chess.move(tempMove);
     if(legalMove) {
         gameSubject.next({board: chess.board() });
     }
+}
+
+
+
+function updateGame(pendingPromotion){
+    const newGame = {
+        board: chess.board(),
+        pendingPromotion
+    };
+    gameSubject.next(newGame);
 }
