3bb52711225453e5d38e43f53ecda32c4a6740ff
zcrossoverz <56151228+zcrossoverz@users.noreply.github.com>
2021-10-31 09:02:31 +0700

    setup when game over

--- src/App.js
+++ src/App.js
@@ -5,15 +5,31 @@ import "./Style.css";
 
 function App() {
   const [board, setBoard] = useState([]);
+  const [isGameOver, setisGameOver] = useState();
+  const [result, setResult] = useState();
   useEffect(() => {
     initGame();
-    const subscribe = gameSubject.subscribe(game => setBoard(game.board));
+    const subscribe = gameSubject.subscribe(({board, isGameOver, result}) => {
+      setBoard(board);
+      setisGameOver(isGameOver);
+      setResult(result);
+    });
     return () => subscribe.unsubscribe();
   }, []);
   return (
     <div className="main">
       <div className="container">
         <Board board={board} />
+      </div>
+      <div className="status">
+        {
+        isGameOver && (
+          <div>
+          <h2>GAME OVER</h2>
+          <p>{result}</p>
+          </div>
+        )
+      }
       </div>
     </div>
   );
--- src/Game.js
+++ src/Game.js
@@ -1,11 +1,15 @@
 import * as Chess from "chess.js";
 import {BehaviorSubject} from "rxjs";
 
-let promotion = 'rnb2bnr/pppPkppp/8/4p3/7q/8/PPPP1PPP/RNBQKBNR w KQ - 1 5';
-const chess = new Chess(promotion);
+ let promotion = 'rnb2bnr/pppPkppp/8/4p3/7q/8/PPPP1PPP/RNBQKBNR w KQ - 1 5';
+ let stateMate = '4k3/4P3/4K3/8/8/8/8/8 b - - 0 78';
+ let checkMate = 'rnb1kbnr/pppp1ppp/8/4p3/5PPq/8/PPPPP2P/RNBQKBNR w KQkq - 1 2';
+ let insuficcientMaterial = 'k7/8/n7/8/8/8/8/7K b - - 0 1';
+
+const chess = new Chess(checkMate);
 
 export const gameSubject = new BehaviorSubject({
-    board: chess.board()
+    board: chess.board(),
 });
 
 export function initGame() {
@@ -16,7 +20,7 @@ export function handleMove(from, to) {
     const promotions = chess.moves({verbose:true}).filter(m => m.promotion);
     console.table(promotions);
     if(promotions.some(p => `${p.from}:${p.to}` === `${from}:${to}`)){
-        console.log('ok');
+        // console.log('ok');
         const pendingPromotion = {from, to, color: promotions[0].color};
         updateGame(pendingPromotion);
     }
@@ -34,16 +38,41 @@ export function move(from, to, promotion) {
     // console.log(from,to);
     const legalMove = chess.move(tempMove);
     if(legalMove) {
-        gameSubject.next({board: chess.board() });
+        updateGame()
     }
 }
 
 
 
 function updateGame(pendingPromotion){
+    const isGameOver = chess.game_over();
+    if(chess.in_check()) console.log('in check');
+    if(chess.in_checkmate()) console.log('in checkmate');
     const newGame = {
         board: chess.board(),
-        pendingPromotion
+        pendingPromotion,
+        isGameOver,
+        result: isGameOver ? getGameResult() : null
     };
     gameSubject.next(newGame);
+}
+
+function getGameResult() {
+        if(chess.in_checkmate()) {
+            const winner = chess.turn() === "w" ? "BLACK" : "WHITE";
+            return `CHECKMATE - WINNER - ${winner}`;
+        } else if (chess.in_draw()) {
+            // console.log('hoa');
+            let reason = '50 - MOVES - RULE';
+            if(chess.in_stalemate()) {
+                reason = 'STALEMATE';
+            }else if(chess.in_threefold_repetition()){
+                reason = 'REPETITION';
+            }else if(chess.insufficient_material()){
+                reason = 'INSUFFICIENT MATERIAL';
+            }
+            return `DRAW - ${reason}`;
+        }else{
+            return 'UNKNOWN REASON';
+        }
 }
